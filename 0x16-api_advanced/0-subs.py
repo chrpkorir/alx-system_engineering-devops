22e3b7d6f8ce7fe267c81eda68cb5eb1b0d550d9
chrpkorir <faythkoko@gmail.com>
2022-11-01 14:35:41 +0000

    Querries and returns no. of Reddit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Function to return number of subscribers'''
+
+    url = "https://www.reddit.com/r/"
+    headers = {"User-Agent": "User Agent"}
+    response = requests.get(url + subreddit + "/about.json",
+                            headers=headers, allow_redirects=False)
+
+    # Print (response)
+    if response.status_code == 200:
+        x = response.json().get("data").get("subscribers")
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
